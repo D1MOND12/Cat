16a4caeb10008e9b6c7243b19c99e90ec115908a
Дмитрий Михеев <234.dimon1@gmail.com>
2020-11-09 11:19:30 +1000

    123

---
+++ Cat/Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cat
+{
+    class Cat
+    {
+        public Cat(string name, DateTime berthday)
+    {
+
+        Name = name;
+        Berthday = berthday;
+
+
+    }
+
+    public string Name
+    {
+        get;
+
+        set;
+    }
+    public void MakeNoise()
+    {
+
+        Console.WriteLine($"{Name} мяукает");
+    }
+    public DateTime Berthday
+    {
+        get;
+
+        set;
+
+    }
+    public int GetAge()
+    {
+        return (DateTime.Today - Berthday).Days / 365;
+
+
+    }
+}
+}
---
+++ Cat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat("Василий Петрович", new DateTime(2000, 12, 12));
+            c.MakeNoise();
+            Console.WriteLine($"Коту {c.Name} {c.GetAge()} лет");
+        }
+    }
+}
