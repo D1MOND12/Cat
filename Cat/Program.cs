4b96850426b2533ded2292e96550087af7f33ba4
Дмитрий Михеев <234.dimon1@gmail.com>
2020-11-09 11:35:49 +1000

    123

--- Cat/Program.cs
+++ Cat/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+
 namespace Cat
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
             Cat c = new Cat("Василий Петрович", new DateTime(2000, 12, 12));
+
             c.MakeNoise();
-            Console.WriteLine($"Коту {c.Name} {c.GetAge()} лет");
+
+            Console.WriteLine($"Кошке по имени {c.Name} уже {c.GatAge()} лет");
+
+            Cat ct = new Cat("Петр Василич", new DateTime(2003, 12, 12));
+
+            ct.MakeNoise();
+
+            Console.WriteLine($"Кошке по имени {ct.Name} уже {ct.GatAge()} лет");
+
+            CatSmartHouse catSmartH = new CatSmartHouse(1000);
+
+            catSmartH.AddCat(c);
+
+            catSmartH.AddCat(ct);
+
+            Console.ReadLine();//123
+
         }
+
     }
+
 }
---
+++ Cat/Smart house.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace Cat
+
+{
+
+    public class CatSmartHouse
+
+    {
+
+        List<Cat> cats = new List<Cat>();
+
+        public CatSmartHouse(int foodResource)
+
+        {
+
+            FoodResource = foodResource;
+
+        }
+
+        public int FoodResource
+
+        { get; set; }
+
+        public void AddCat(Cat cat)
+
+        {
+
+            cats.Add(cat);
+
+            cat.HungerStatusChenged += Cat_HungerStatusChenged;
+
+        }
+
+        private void Cat_HungerStatusChenged(object sender, EventArgs e)
+
+        {
+
+            var cat = (Cat)sender;
+
+            if (cat.HungryStatus <= 20 && FoodResource > 0)
+
+            {
+
+                byte needFood = (byte)(100 - cat.HungryStatus);
+
+                if (FoodResource > needFood)
+
+                    FoodResource -= needFood;
+
+                else
+
+                {
+
+                    needFood = (byte)FoodResource;
+
+                    FoodResource = 0;
+
+                }
+
+                cat.Feed(needFood);
+
+                Console.WriteLine($"Покормлена кошка: {cat.Name}\nОстаток еды в вольере: {FoodResource}");
+
+            }
+
+        }
+
+    }
+
+}
